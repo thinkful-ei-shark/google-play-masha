43142915910a054144f3c5f2f3071cc331f75c21
Marc Trius <derpayatz@gmail.com>
2020-10-20 10:48:21 -0500

    param validation

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ const app = express();
 app.use(morgan('common'));
 
 app.get('/apps', (req, res) => {
-  const {sort, genres} = req.query;
+  const { sort, genres } = req.query;
   if (!sort && !genres) return res.json(playStore);
+  if (sort && !['rating', 'app'].includes(sort)) {
+    return res
+      .status(400)
+      .json({ message: 'sort must be on of "rating" or "app"' });
+  }
+  if (genres &&
+    !['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
+      .includes(genres)) {
+    return res
+      .status(400)
+      .json({ message: 'genres must be one of "Action", "Puzzle", "Strategy", "Casual", "Arcade", or "Card"' });
+  }
 });
 
 app.listen(8080, () => console.log('Server on 8080'));
