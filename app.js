3d8efc7165819440d5bc746f16d31133ebaa8131
Marc Trius <derpayatz@gmail.com>
2020-10-20 10:40:55 -0500

    default response

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const playStore = require('./playstore');
 
 const app = express();
 
 app.use(morgan('common'));
+
+app.get('/apps', (req, res) => {
+  const {sort, genres} = req.query;
+  if (!sort && !genres) return res.json(playStore);
+});
 
 app.listen(8080, () => console.log('Server on 8080'));
