725d83ec7b9403283f53392f70ad4e25dace1eca
Marc Trius <derpayatz@gmail.com>
2020-10-20 10:54:43 -0500

    implement sort

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(morgan('common'));
 app.get('/apps', (req, res) => {
   const { sort, genres } = req.query;
   if (!sort && !genres) return res.json(playStore);
-  if (sort && !['rating', 'app'].includes(sort)) {
+  if (sort && !['Rating', 'App'].includes(sort)) {
     return res
       .status(400)
       .json({ message: 'sort must be on of "rating" or "app"' });
@@ -21,6 +21,10 @@ app.get('/apps', (req, res) => {
       .status(400)
       .json({ message: 'genres must be one of "Action", "Puzzle", "Strategy", "Casual", "Arcade", or "Card"' });
   }
+  if(sort) {
+    playStore.sort((a, b) => a[sort] > b[sort] ? -1 : 1);
+  }
+  return res.json(playStore);
 });
 
 app.listen(8080, () => console.log('Server on 8080'));
