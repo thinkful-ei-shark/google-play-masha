c845018af42c48329260bbf8ce99666fb2506eb4
Marc Trius <derpayatz@gmail.com>
2020-10-21 14:20:37 -0500

    implement no params test

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,25 @@ const supertest = require('supertest');
 const app = require('../app');
 const expect = require('chai').expect;
 
+function expectAppObj(obj) {
+  return expect(obj).to.include.all.keys('App', 'Category', 'Rating');
+}
+
 describe('GET /apps endpoint', () => {
   it('incorrect parameter', () => {
     return supertest(app)
       .get('/apps')
       .query({foo:'bar'})
-      .expect(400)
-  })
-})
+      .expect(400);
+  });
+  it('no params expect 200 and json list of games', () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        expect(res.body).to.be.an('array');
+        expectAppObj(res.body[0]);
+      });
+  });
+});
