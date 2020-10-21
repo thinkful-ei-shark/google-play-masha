0b193f50fe2946354a71a6803a940afb2448d9a7
Marc Trius <derpayatz@gmail.com>
2020-10-21 14:41:12 -0500

    implement test for sorting

--- test/app.test.js
+++ test/app.test.js
@@ -10,7 +10,7 @@ describe('GET /apps endpoint', () => {
   it('incorrect parameter', () => {
     return supertest(app)
       .get('/apps')
-      .query({foo:'bar'})
+      .query({ foo: 'bar' })
       .expect(400);
   });
   it('no params expect 200 and json list of games', () => {
@@ -23,4 +23,37 @@ describe('GET /apps endpoint', () => {
         expectAppObj(res.body[0]);
       });
   });
+  describe('sorting functions', () => {
+    it('expect 400 incorrect value', () => {
+      return supertest(app)
+        .get('/apps')
+        .query({ sort: 'foo' })
+        .expect(400);
+    });
+    ['Rating', 'App'].forEach(term => {
+      it(`expect list sorted by ${term}`, () => {
+        return supertest(app)
+          .get('/apps')
+          .query({ sort: term })
+          .expect(200)
+          .then((res) => {
+            expect(res.body).to.be.an('array');
+            expectAppObj(res.body[0]);
+            let ordered = true;
+            let i = 0;
+            while (ordered, i < res.body.length - 1) {
+              ordered = res.body[i][term] >= res.body[i + 1][term];
+              i++;
+            }
+            expect(ordered).to.be.true;
+          });
+      });
+    });
+
+
+  });
+  describe('filtering functions', () => {
+    
+  });
+  describe('sorting and filtering', () => { });
 });
